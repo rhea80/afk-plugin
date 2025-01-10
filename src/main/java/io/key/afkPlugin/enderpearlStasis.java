2dbb628d174971cfa0af07468e67b8ba90bbb339
rhea80 <rheaa181@gmail.com>
2025-01-10 00:12:30 -0500

    make sure the player cannot move in spectator mode.

--- src/main/java/io/key/afkPlugin/enderpearlStasis.java
+++ src/main/java/io/key/afkPlugin/enderpearlStasis.java
@@ -1,17 +1,20 @@
 package io.key.afkPlugin;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.UUID;
 
-public final class enderpearlStasis extends JavaPlugin {
+public final class enderpearlStasis extends JavaPlugin implements @NotNull Listener {
 
     // Stores UUID and GameMode of the player
     private final HashMap<UUID, GameMode> playerGameModes = new HashMap<>();
@@ -20,6 +23,7 @@ public final class enderpearlStasis extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("AFK Plugin Enabled!");
+        Bukkit.getPluginManager().registerEvents(this, this);
 
     }
 
@@ -42,20 +46,31 @@ public final class enderpearlStasis extends JavaPlugin {
                     // unAFK the player.
                     GameMode previousMode = playerGameModes.getOrDefault(playerId, GameMode.SURVIVAL);
                     player.setGameMode(previousMode);
-                    player.sendMessage("You are no longer AFK.");
-                    playerGameModes.remove(playerId); // Clear their previous mode from memory
+                    player.sendMessage("You are no longer AFK");
+                    playerGameModes.remove(playerId);
                 } else {
                     // set player to Spectator mode
                     playerGameModes.put(playerId, player.getGameMode());
                     player.setGameMode(GameMode.SPECTATOR);
-                    player.sendMessage("You are now AFK. ");
+                    player.sendMessage("You are now AFK");
                 }
                 return true;
             } else {
-                sender.sendMessage("Only players can use this command.");
+                sender.sendMessage("Unknown person. Try again later.");
                 return true;
             }
         }
         return false;
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (player.getGameMode() == GameMode.SPECTATOR && playerGameModes.containsKey(player.getUniqueId())) {
+            if (!event.getFrom().getBlock().equals(event.getTo().getBlock())) {
+                event.setTo(event.getFrom());
+                player.sendMessage("You cannot move while AFK.");
+            }
+        }
     }
 }
